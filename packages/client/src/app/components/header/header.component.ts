6e473e71980c6504a3545e2b27443b93cc58f2d3
James <ptrsnja@gmail.com>
2023-07-20 10:32:42 -0400

    APP-36 Mobile warning on splash screen

--- packages/client/src/app/components/header/header.component.ts
+++ packages/client/src/app/components/header/header.component.ts
@@ -4,12 +4,13 @@ import { NavigationEnd, NavigationStart, Route, Router } from '@angular/router';
 import { Platform } from '@angular/cdk/platform';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Store, select } from '@ngrx/store';
-import { Subject, filter, map, takeUntil } from 'rxjs';
-import { AppState } from '../../models/app.model';
+import { Subject, filter, map, switchMap, takeUntil } from 'rxjs';
+import { AppPlatform, AppState } from '../../models/app.model';
 import { RecognitionStatus } from '../../models/recognition.model';
 import { recognitionStatusSelector } from '../../selectors/recognition.selector';
 import { MenuItem } from './header.model';
 import { animate, style, transition, trigger } from '@angular/animations';
+import { platformSelector } from '../../selectors/app.selector';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -29,13 +30,12 @@ export class HeaderComponent implements OnInit, OnDestroy {
   @ViewChild('menu', {read: ElementRef}) menuElement!: ElementRef;
   public menuItems: MenuItem[];
   public activeRoute: WritableSignal<string>;
-  public showRecordButton: WritableSignal<boolean> = signal(true);
+  public showRecordButton: Signal<boolean | undefined>;
   public isActive: Signal<boolean | undefined>;
-
+  
   private onDestroy$: Subject<void> = new Subject<void>();
 
   constructor(private router: Router,
-              private platform: Platform,
               private store: Store<AppState>,
               private renderer: Renderer2) {
     
@@ -56,7 +56,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
         label: route.data?.['name'] || route.path,
         routerOutlet: `/${route.path}`
       }
-    })
+    });
+    this.showRecordButton = toSignal(this.store.pipe(select(platformSelector), map((platform) => platform === AppPlatform.desktop)))
   }
 
   ngOnInit(): void {
@@ -66,17 +67,9 @@ export class HeaderComponent implements OnInit, OnDestroy {
     ).subscribe((ev: NavigationEnd) => {
       this.activeRoute.set(ev.url);
     });
-
-    this._checkPlatform();
   }
 
   ngOnDestroy(): void {
     this.onDestroy$.next();
-  }
-
-  private _checkPlatform(): void {
-    if (this.platform.ANDROID || this.platform.IOS) {
-      this.showRecordButton.set(false);
-    }
   }
 }
