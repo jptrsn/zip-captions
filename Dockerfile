8f8fed1af14dacf1d20b6891948aa905858bfa73
James <ptrsnja@gmail.com>
2023-08-01 18:24:01 -0400

    APP-44 Updated github actions, angular environment variables

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,27 @@ ARG IMAGE=node:lts-alpine
 
 FROM $IMAGE as base_image
 WORKDIR /usr/src/app
-COPY . .
+COPY package*.json .
 ARG NX_NON_NATIVE_HASHER=true
 RUN npm ci
 
 # DEVELOPMENT
 FROM base_image as dev
+COPY . .
 CMD [""]
 
 # STAGING
-FROM base_image as staging_client
+FROM dev as staging_build_client
 RUN npm run build:client
 
-FROM nginx:alpine as staging
-COPY --from=staging_client /usr/src/app/docs /usr/share/nginx/html
+FROM dev as staging_build_server
+RUN npm run build:signal
+
+FROM nginx:alpine as staging_client
+COPY --from=staging_build_client /usr/src/app/docs /usr/share/nginx/html
 EXPOSE 80
 
+FROM staging_build_server as staging_server
+CMD ["node", "./dist/packages/signal/main.js"]
+EXPOSE 3000 9000
 
---
+++ packages/client/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+function getClientEnvironment() {
+  // Grab NX_* environment variables and prepare them to be injected
+  // into the application via DefinePlugin in webpack configuration.
+  const NX_APP = /^ZIP_/i;
+
+  const raw = Object.keys(process.env)
+    .filter((key) => NX_APP.test(key))
+    .reduce((env, key) => {
+      env[key] = process.env[key];
+      return env;
+    }, {});
+
+  // Stringify all values so we can feed into webpack DefinePlugin
+  return {
+    'process.env': Object.keys(raw).reduce((env, key) => {
+      env[key] = JSON.stringify(raw[key]);
+      return env;
+    }, {}),
+  };
+}
+
+module.exports = (config, options, context) => {
+  // Overwrite the mode set by Angular if the NODE_ENV is set
+  config.mode = process.env.NODE_ENV || config.mode;
+  config.plugins.push(new webpack.DefinePlugin(getClientEnvironment()));
+  return config;
+};
