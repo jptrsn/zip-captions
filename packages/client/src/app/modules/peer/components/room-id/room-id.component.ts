4c3e1e1d6cb9babeeac779dd8671d084d7c125f7
James <ptrsnja@gmail.com>
2023-08-23 12:10:38 -0400

    APP-54 Copy session ID / join code

--- packages/client/src/app/modules/peer/components/room-id/room-id.component.ts
+++ packages/client/src/app/modules/peer/components/room-id/room-id.component.ts
@@ -1,10 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, WritableSignal, signal } from '@angular/core';
+import { fadeOutOnLeaveAnimation, slideInUpOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-room-id',
   templateUrl: './room-id.component.html',
   styleUrls: ['./room-id.component.scss'],
+  animations: [
+    slideInUpOnEnterAnimation(),
+    fadeOutOnLeaveAnimation()
+  ]
 })
 export class RoomIdComponent {
   @Input() roomId?: string;
+  public showToast: WritableSignal<boolean> = signal(false);
+
+  showCopiedToClipboard(): void {
+    this.showToast.set(true);
+    setTimeout(() => this.showToast.set(false), 1500)
+  }
 }
--- packages/client/src/app/modules/peer/peer.module.ts
+++ packages/client/src/app/modules/peer/peer.module.ts
@@ -1,27 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgIconsModule } from '@ng-icons/core';
 import { EffectsModule } from '@ngrx/effects';
 import { TranslateModule } from '@ngx-translate/core';
+import { ClipboardModule } from 'ngx-clipboard';
 import { PeerEffects } from '../../effects/peer.effects';
 import { MediaModule } from '../media/media.module';
+import { AcceptPeerConnectionsModalComponent } from './components/accept-peer-connections-modal/accept-peer-connections-modal.component';
+import { BroadcastControlsSidebarComponent } from './components/broadcast-controls-sidebar/broadcast-controls-sidebar.component';
+import { BroadcastRenderComponent } from './components/broadcast-render/broadcast-render.component';
+import { BroadcastRoomComponent } from './components/broadcast-room/broadcast-room.component';
 import { ConnectedPeerCountChipComponent } from './components/connected-peer-count-chip/connected-peer-count-chip.component';
 import { EndBroadcastComponent } from './components/end-broadcast/end-broadcast.component';
+import { JoinCodeComponent } from './components/join-code/join-code.component';
 import { LeaveSessionComponent } from './components/leave-session/leave-session.component';
 import { PeerLandingComponent } from './components/peer-landing/peer-landing.component';
 import { PeerServerStatusChipComponent } from './components/peer-server-status-chip/peer-server-status-chip.component';
+import { RoomIdComponent } from './components/room-id/room-id.component';
 import { SocketServerStatusChipComponent } from './components/socket-server-status-chip/socket-server-status-chip.component';
 import { ViewBroadcastComponent } from './components/view-broadcast/view-broadcast.component';
 import { PeerRoutingModule } from './peer-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RoomIdComponent } from './components/room-id/room-id.component';
-import { JoinCodeComponent } from './components/join-code/join-code.component';
-import { BroadcastRoomComponent } from './components/broadcast-room/broadcast-room.component';
-import { BroadcastControlsSidebarComponent } from './components/broadcast-controls-sidebar/broadcast-controls-sidebar.component';
-import { BroadcastRenderComponent } from './components/broadcast-render/broadcast-render.component';
-import { AcceptPeerConnectionsModalComponent } from './components/accept-peer-connections-modal/accept-peer-connections-modal.component';
 
 @NgModule({
   declarations: [
@@ -48,6 +49,7 @@ import { AcceptPeerConnectionsModalComponent } from './components/accept-peer-co
     TranslateModule.forChild({ extend: true }),
     EffectsModule.forFeature([PeerEffects]),
     MediaModule,
+    ClipboardModule,
   ],
 })
 export class PeerModule {}
