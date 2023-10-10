541f6296e38b03043634c08cdec990f30136538d
James <ptrsnja@gmail.com>
2023-10-10 17:30:21 -0400

    APP-106 Handle disabled onetap & prompt when session

--- packages/client/src/app/modules/auth/components/google-login/google-login.component.ts
+++ packages/client/src/app/modules/auth/components/google-login/google-login.component.ts
@@ -10,43 +10,64 @@ declare let google: any;
 })
 export class GoogleLoginComponent implements OnInit {
   @ViewChild('signInButton', {read: ElementRef}) button!: ElementRef<HTMLDivElement>;
-  loaded: WritableSignal<boolean> = signal(false);
-  private script!: HTMLScriptElement;
+  disabled: WritableSignal<boolean> = signal(false);
+  error: WritableSignal<string | undefined> = signal(undefined);
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
-    this.script = this._loadClientLib();
+    this._loadClientLib();
   }
 
   onLibLoad(): void {
-    console.log('on lib load', google)
+    console.log('onLibLoad', google);
     google.accounts.id.initialize({
       client_id: '374403697962-n3m6fa49ojjub0urrid4ojotff4hhdf2.apps.googleusercontent.com',
-      native_callback: this.onSignIn,
+      callback: this.onSignIn,
       context: 'use',
-      cancel_on_tap_outside: true,
+      cancel_on_tap_outside: false,
       ux_mode: 'popup',
-      allowed_parent_origin: ['https://*.zipcaptions.app', 'http://localhost:4200'],
       itp_support: true,
       use_fedcm_for_prompt: true
     })
-    google.accounts.id.renderButton(this.button.nativeElement, {
-      theme: 'filled_blue', size: 'large', width: '250', type: 'standard'
-    })
-    this.loaded.set(true);
+    google.accounts.id.renderButton(this.document.getElementById('g_signin'), { theme: 'filled_blue', size: 'large', width: '250'})
+    this.prompt();
   }
 
   prompt(): void {
+    this.disabled.set(true);
     google.accounts.id.prompt((notification: any) => {
       console.log('prompt notification', notification);
-    });
+      this.disabled.set((notification.g === 'display'));
+      console.log('isNotDisplayed', notification.isNotDisplayed());
+      console.log('isSkippedMoment', notification.isSkippedMoment());
+      if (notification.isNotDisplayed()) {
+        console.log('j', notification.j);
+        let msg = 'Error';
+        if (notification.j === 'opt_out_or_no_session') {
+          msg = 'opted out, or no session available';
+        } else if (notification.j === 'suppressed_by_user') {
+          msg = 'suppressed by user';
+        }
+        this.error.set(msg)
+      } else if (notification.isSkippedMoment()) {
+        this.error.set('skipped or disabled by user')
+        this.disabled.set(true);
+      } else if (notification.i) {
+        console.log('i', notification.i);
+      }
+    })
   }
 
   onSignIn(googleUser:any): void {
     console.log('onSignIn', googleUser);
+  }
+
+  clearError(): void {
+    this.error.set(undefined);
+    this.disabled.set(false);
   }
 
   private _loadClientLib(): HTMLScriptElement {
@@ -54,6 +75,7 @@ export class GoogleLoginComponent implements OnInit {
     this.renderer.setAttribute(script, 'type', 'text/javascript')
     this.renderer.setAttribute(script, 'src', 'https://accounts.google.com/gsi/client');
     script.onload = () => this.onLibLoad();
+    script.onerror = (err: any) => console.log('script error', err)
     this.renderer.appendChild(this.document.body, script);
     return script;
   }
