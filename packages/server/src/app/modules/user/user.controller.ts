c5c7adaa282b8d325fd86596687ba735c86af052
James Petersen <ptrsnja@gmail.com>
2024-02-19 19:26:14 -0500

    fix hardcoded localhost

--- packages/server/src/app/modules/user/user.controller.ts
+++ packages/server/src/app/modules/user/user.controller.ts
@@ -10,83 +10,86 @@ import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService,
-                private jwtService: JwtService) 
-    {
-    }
+  
+  private clientUrl: string;
+  constructor(private readonly userService: UserService,
+              private jwtService: JwtService) 
+  {
+    this.clientUrl = process.env.APP_ORIGIN
+  }
 
-    @Get('profile')
-    @UseGuards(JwtAuthGuard)
-    async getUser(@Req() req): Promise<UserProfile> {
-      const user = await this.userService.findOne({id: req.user.id});
-      const userProfile = {
-        id: user.id,
-        createdAt: user.createdAt.toString(),
-        familyName: user.familyName,
-        givenName: user.givenName,
-        primaryEmail: user.primaryEmail,
-        googleConnected: !!user.googleId,
-        azureConnected: !!user.msId
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  async getUser(@Req() req): Promise<UserProfile> {
+    const user = await this.userService.findOne({id: req.user.id});
+    const userProfile = {
+      id: user.id,
+      createdAt: user.createdAt.toString(),
+      familyName: user.familyName,
+      givenName: user.givenName,
+      primaryEmail: user.primaryEmail,
+      googleConnected: !!user.googleId,
+      azureConnected: !!user.msId
+    }
+    return userProfile;
+  }
+
+  @Get('google-login')
+  @NoCache()
+  @UseGuards(GoogleOAuthGuard)
+  googleAuth(@Req() req) {
+      console.log('login with google')
+  }
+
+  @Get('azure-login')
+  @NoCache()
+  @UseGuards(AzureOAuthGuard)
+  azureAuth(@Req() req) {
+    console.log('login with azure')
+  }
+
+  @Get('google-redirect')
+  @NoCache()
+  @UseGuards(GoogleOAuthGuard)
+  async googleAuthRedirect(@Req() req, @Res() res) {
+    try {
+      const user = await this.userService.googleLogin(req);
+      this._sendTokenResponse(user.id, res);
+    } catch(e) {
+      res.redirect(`${this.clientUrl}/auth/login?error=${encodeURIComponent(e)}`)
+    }
+  }
+
+  @Get('azure-redirect')
+  @NoCache()
+  @UseGuards(AzureOAuthGuard)
+  async azureAuthRedirect(@Req() req, @Res() res) {
+    try {
+      if (req.authInfo.message) {
+        throw new Error('No user from microsoft');
       }
-      return userProfile;
+      const user = await this.userService.msLogin(req.user);
+      this._sendTokenResponse(user.id, res);
+    } catch(e: any) {
+      res.redirect(`${this.clientUrl}/auth/login?error=${encodeURIComponent(e)}`)
     }
+  }
 
-    @Get('google-login')
-    @NoCache()
-    @UseGuards(GoogleOAuthGuard)
-    googleAuth(@Req() req) {
-        console.log('login with google')
-    }
-
-    @Get('azure-login')
-    @NoCache()
-    @UseGuards(AzureOAuthGuard)
-    azureAuth(@Req() req) {
-      console.log('login with azure')
-    }
-
-    @Get('google-redirect')
-    @NoCache()
-    @UseGuards(GoogleOAuthGuard)
-    async googleAuthRedirect(@Req() req, @Res() res) {
-      try {
-        const user = await this.userService.googleLogin(req);
-        this._sendTokenResponse(user.id, res);
-      } catch(e) {
-        res.redirect(`http://localhost:4200/auth/login?error=${encodeURIComponent(e)}`)
+  @Post('logout')
+  @UseGuards(JwtAuthGuard)
+  async logout(@Req() req, @Res() res) {
+    await req.logout((err) => {
+      if (err) {
+        console.warn(err)
+        Promise.reject(err)
       }
-    }
+      Promise.resolve('OK')
+    })
+    res.status(200).send('OK')
+  }
 
-    @Get('azure-redirect')
-    @NoCache()
-    @UseGuards(AzureOAuthGuard)
-    async azureAuthRedirect(@Req() req, @Res() res) {
-      try {
-        if (req.authInfo.message) {
-          throw new Error('No user from microsoft');
-        }
-        const user = await this.userService.msLogin(req.user);
-        this._sendTokenResponse(user.id, res);
-      } catch(e: any) {
-        res.redirect(`http://localhost:4200/auth/login?error=${encodeURIComponent(e)}`)
-      }
-    }
-
-    @Post('logout')
-    @UseGuards(JwtAuthGuard)
-    async logout(@Req() req, @Res() res) {
-      await req.logout((err) => {
-        if (err) {
-          console.warn(err)
-          Promise.reject(err)
-        }
-        Promise.resolve('OK')
-      })
-      res.status(200).send('OK')
-    }
-
-    private _sendTokenResponse(userId: string, res: Response): void {
-      const token = this.jwtService.sign({ sub: userId })
-      res.redirect(`http://localhost:4200/auth/login?id_token=${token}`);
-    }
+  private _sendTokenResponse(userId: string, res: Response): void {
+    const token = this.jwtService.sign({ sub: userId })
+    res.redirect(`${this.clientUrl}/auth/login?id_token=${token}`);
+  }
 }
