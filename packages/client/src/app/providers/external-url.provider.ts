b8cbbbc7778f145fd76d06b036b3304af94cd8de
James Petersen <ptrsnja@gmail.com>
2025-02-16 17:24:54 -0500

    external URL route provider

---
+++ packages/client/src/app/providers/external-url.provider.ts
@@ -0,0 +1,18 @@
+import { InjectionToken } from "@angular/core";
+import { ActivatedRouteSnapshot } from "@angular/router";
+
+export const externalUrlProvider = new InjectionToken('externalUrlRedirectResolver');
+
+export const provideExternalUrls = () => {
+  return {
+    provide: externalUrlProvider,
+    useValue: (route: ActivatedRouteSnapshot) => {
+      console.log('externalUrlProvider', route.data);
+      const externalUrl = route.data['externalUrl'] || route.data['url'];
+      if (!externalUrl) {
+        throw new Error('No external URL provided')
+      }
+      window.location.replace(externalUrl);
+    }
+  }
+}
